b28bdd54642d76a03217cc2547b937f0b4e5ff0d
Vamsee Mupparapu <vamsee@Vamsees-MBP.cable.rcn.com>
2021-10-13 06:59:35 -0400

    Heroku port config

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 const port = 80;
 app.use(bodyParser.urlencoded({ extended: true }))
 
-app.listen(port, () => {
+app.listen(process.env.PORT || port, () => {
   console.log("Application starting on port " + port);
 });
 
