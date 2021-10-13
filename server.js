a654a5554dbc9b9d9518935e23bcf57420aa6f88
Vamsee Mupparapu <vamsee@Vamsees-MBP.cable.rcn.com>
2021-10-13 06:47:15 -0400

    Changing to port 80 for Heroku Deployment

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-const port = 8000;
+const port = 80;
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.listen(port, () => {
